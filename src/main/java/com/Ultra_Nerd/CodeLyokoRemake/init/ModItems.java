70a00729d0902cb3dfe66a375738e4fdf7b39091
xana43 <jaylen@woontech.com>
2019-12-04 10:53:19 -0500

    added more stuff, check in eclipse for errors

--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake/init/ModItems.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake/init/ModItems.java
@@ -37,12 +37,16 @@ public class ModItems
  public static final ToolMaterial MATERIAL_QUANTUM = EnumHelper.addToolMaterial("material_quantum", 20, 250, 7.0f, 3.0f, 900);
  public static final ToolMaterial MATERIAL_WARRIOR = EnumHelper.addToolMaterial("material_warrior", 4, 4000, 20, 30, 9000);
  public static final ToolMaterial MATERIAL_FELINE = EnumHelper.addToolMaterial("material_feline", 4, 2000, 900, 6, 90);
+ public static final ToolMaterial MATERIAL_NINJA = EnumHelper.addToolMaterial("material_ninja", 4, 2000, 9000, 6, 90);
+ public static final ToolMaterial MATERIAL_SAMURAI = EnumHelper.addToolMaterial("material_samurai" , 4 1000, 10, 25, 80);
+ public static final ToolMaterial MATERIAL_GUARDIAN = EnumHelper.addToolMaterial("material_guardian, 4, 4000, 40, 60, 100);
  public static final ArmorMaterial ARMOR_MATERIAL_ODD = EnumHelper.addArmorMaterial("armor_material_odd", ref.MOD_ID + ":odd", 30, new int[] {6, 12, 16, 6}, 20, SoundEvents.ITEM_ARMOR_EQIIP_ELYTRA, 0.0f);
  public static final ArmorMaterial ARMOR_MATERIAL_YUMI = EnumHelper.addArmorMaterial("armor_material_yumi", ref.MOD_ID + ":yumi", 30, new int[] {6, 12, 16, 6}, 20, SoundEvents.ITEM_ARMOR_EQIIP_ELYTRA, 0.0f);
  public static final ArmorMaterial ARMOR_MATERIAL_AELITA = EnumHelper.addArmorMaterial("armor_material_aelita", ref.MOD_ID + ":aelita", 30, new int[] {6, 12, 16, 6}, 20, SoundEvents.ITEM_ARMOR_EQIIP_ELYTRA, 0.0f);
  public static final ArmorMaterial ARMOR_MATERIAL_ULRICH = EnumHelper.addArmorMaterial("armor_material_ulrich", ref.MOD_ID + ":ulrich", 30, new int[] {4, 16, 12, 9}, 20, SoundEvents.ITEM_ARMOR_EQIIP_ELYTRA, 0.0f);
  public static final ArmorMaterial ARMOR_MATERIAL_WILLIAM = EnumHelper.addArmorMaterial("armor_material_william", ref.MOD_ID + ":william", 30, new int[] {24, 24, 24, 24}, 20, SoundEvents.ITEM_ARMOR_EQIIP_ELYTRA, 0.0f);
  public static final ArmorMaterial ARMOR_MATERIAL_BITS = EnumHelper.addArmorMaterial("armor_material_bits", ref.MOD_ID + ":bits", 30, new int[] {16, 16, 16, 16}, 20, SoundEvents.ITEM_ARMOR_EQIIP_ELYTRA, 0.0f);
+ public static final ArmorMaterial AROMOR_MATERIAL_JEREMY = EnumHelper.addArmorMaterial("armor_material_jeremy", ref.MOD_ID + ":jeremy", 30, new int[]{3,3,3,3}, 20, SoundEvents.ITEM_ARMROR_EQIIP_ELYTRA, 0.0f);
  //Items
  public static final Item SOLID_QUANTUM = new ItemBase("solid_quantum");
 
@@ -68,12 +72,14 @@ public class ModItems
  public static final Item PETABYTE = new ItemBase("PetaByte");
  public static final Item QUBIT = new ItemBase("qubit");
  public static final Item QUBYTE = new ItemBase("QuByte");
-
+ public static final Item TITANIUM = new ItemBase("titanium");
+ 
  //tools
  public static final ItemSword ZWEIHANDER = new Zweihander("zweihander", MATERIAL_WARRIOR);
  public static final ItemSword QUANTUM_SABER = new QuantSword("quantum_saber", MATERIAL_QUANTUM);
  public static final ItemSpade DIGITAL_SAMPLER = new QuantSpade("digital_sampler", MATERIAL_QUANTUM);
- public static final Item LASER_ARROWSHOOTER = new LaserArrowShooter("laser_arrowshooter"); 
+ public static final Item LASER_ARROWSHOOTER = new LaserArrowShooter("laser_arrowshooter");
+ public static final ItemSword DIGITAL_SABER = new QuantSword("digital_saber", MATERIAL_SAMURAI);  
  //armor
  public static final Item DIGITAL_ENVELOPE_CAP = new ArmorBase("digital_envelope_cap", ARMOR_MATERIAL_BITS, 1 , EntityEquipmentSlot.HEAD);
  public static final Item AELITA_CHESTPLATE = new ArmorBase("aelita_chestplate", ARMOR_MATERIAL_AELITA, 1 , EntityEquipmentSlot.CHEST);
@@ -91,7 +97,7 @@ public class ModItems
  public static final Item WILLIAM_CHESTPLATE = new ArmorBase("william_chestplate", ARMOR_MATERIAL_WILLIAM, 1 , EntityEquipmentSlot.CHEST);
  public static final Item WILLIAM_LEGGINGS = new ArmorBase("william_leggings", ARMOR_MATERIAL_WILLIAM, 2 , EntityEquipmentSlot.LEGS);
  public static final Item WILLIAM_BOOTS = new ArmorBase("william_boots", ARMOR_MATERIAL_WILLIAM, 1 , EntityEquipmentSlot.FEET);
- public static ItemRecord LYOKO; 
+ public static ItemRecord LYOKO;
  public static void initRecords()
  {
 	 LYOKO = new RecordBase("lyoko", Souinds.THEME);
