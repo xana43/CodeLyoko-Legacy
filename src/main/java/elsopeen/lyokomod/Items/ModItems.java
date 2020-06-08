48b2de26c448ac5ac79fb651e1bfdaa80698fed6
Elsopéen/PINON Grégory <elsopeen@gmail.com>
2020-06-08 16:36:01 +0200

    Surface territories blocks added. TODO Carthage blocks, Lyoko Interface Block (NO GUI)

--- src/main/java/elsopeen/lyokomod/Items/ModItems.java
+++ src/main/java/elsopeen/lyokomod/Items/ModItems.java
@@ -15,6 +15,9 @@ public class ModItems {
 
     //Register the tutorial dust with "tutorial_dust" as registry name and default properties
     public static final RegistryObject<Item> MOUNTAIN_ROCK = ITEMS.register("mountain_rock", () -> new BlockItem(ModBlocks.MOUNTAIN_BLOCK.get(), new Item.Properties()));
-
+    public static final RegistryObject<Item> FOREST_GRASS = ITEMS.register("forest_grass", () -> new BlockItem(ModBlocks.FOREST_GRASS.get(), new Item.Properties()));
+    public static final RegistryObject<Item> FOREST_LOG = ITEMS.register("forest_log", () -> new BlockItem(ModBlocks.FOREST_LOG.get(), new Item.Properties()));
+    public static final RegistryObject<Item> DESERT_SAND = ITEMS.register("desert_sand", () -> new BlockItem(ModBlocks.DESERT_SAND.get(), new Item.Properties()));
+    public static final RegistryObject<Item> BANQUISE_ICE = ITEMS.register("banquise_ice", () -> new BlockItem(ModBlocks.BANQUISE_ICE.get(), new Item.Properties()));
 
 }
