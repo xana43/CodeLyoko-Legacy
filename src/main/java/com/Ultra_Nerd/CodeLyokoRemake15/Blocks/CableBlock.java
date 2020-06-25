98bfb20044b6a447f4652dddb8924abcf0f7415a
xana43 <jaylen@woontech.com>
2020-06-25 10:34:17 -0400

    Update CableBlock.java

--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/Blocks/CableBlock.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/Blocks/CableBlock.java
@@ -1,17 +1,20 @@
 package com.Ultra_Nerd.CodeLyokoRemake15.Blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.util.datafix.fixes.ChunkPaletteFormat.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.IBlockReader;
 import net.minecraftforge.common.ToolType;
 
 public class CableBlock extends Block {
 	
-	public static final AxisAlignedBB CABLEAABB = new AxisAlignedBB(0.3125D, 0, 0.3125D, 0.8125D, 1.25D, 0.8125D);
+	public static final DirectionProperty DIR = HorizontalBlock.HORIZONTAL_FACING;
 	
 	public CableBlock()
 	{
@@ -23,25 +26,24 @@ super(Block.Properties.create(Material.ROCK)
 				.harvestLevel(2)
 				.harvestTool(ToolType.PICKAXE)
 				);
-		
+		this.setDefaultState(this.stateContainer.getBaseState().with(DIR, Direction.NORTH));
 	}
 	
 	@Override
-	public boolean isOpaqueCube(IBlockState state) {
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		// TODO Auto-generated method stub
+		return this.getDefaultState().with(DIR, context.getPlacementHorizontalFacing().getOpposite());
+	}
+	
+	@Override
+	public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
 	@Override
-	public boolean isFullCube(IBlockState state) {
+	public boolean isTransparent(BlockState state) {
 		// TODO Auto-generated method stub
-		return false;
-	}
-	
-	@Override
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-		// TODO Auto-generated method stub
-		return CABLEAABB;
+		return true;
 	}
 	
 	
