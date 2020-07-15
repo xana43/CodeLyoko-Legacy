8657d408b644f57deb298abfdd403bddf3866fcd
xana43 <jaylen@woontech.com>
2020-07-15 05:53:35 -0400

    tried to make sector 5 have a sphereical generation

--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/world/generators/Sector5Generator.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/world/generators/Sector5Generator.java
@@ -1,54 +1,73 @@
 package com.Ultra_Nerd.CodeLyokoRemake15.world.generators;
 
+import com.Ultra_Nerd.CodeLyokoRemake15.init.ModBlocks;
+import com.Ultra_Nerd.CodeLyokoRemake15.init.ModFluids;
 import com.Ultra_Nerd.CodeLyokoRemake15.world.dimension.Carthadge.Sector5GenSettings;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.biome.provider.BiomeProvider;
-import net.minecraft.world.gen.NoiseChunkGenerator;
+import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.gen.WorldGenRegion;
 
-public class Sector5Generator extends NoiseChunkGenerator<Sector5GenSettings> {
+public class Sector5Generator extends ChunkGenerator<Sector5GenSettings> {
 
 
     public Sector5Generator(IWorld worldIn, BiomeProvider biomeProvidein, Sector5GenSettings settingsIn) {
-        super(worldIn, biomeProvidein, 8, 4, 128, settingsIn, true);
+        super(worldIn, biomeProvidein, settingsIn);
     }
+
+    @Override
+    public void generateSurface(WorldGenRegion p_225551_1_, IChunk chunk) {
+        BlockState bedrock = ModFluids.DIO.get().getDefaultState();
+        BlockState stone = ModBlocks.SECTOR_5.get().getDefaultState();
+        ChunkPos chunkpos = chunk.getPos();
+
+        BlockPos.Mutable pos = new BlockPos.Mutable();
+
+        int x;
+        int z;
+
+        for (x = 0; x < 16; x++) {
+            for (z = 0; z < 16; z++) {
+                chunk.setBlockState(pos.setPos(x, 0, z), bedrock, false);
+            }
+        }
+
+        for (x = 0; x < 16; x++) {
+            for (z = 0; z < 16; z++) {
+                int realx = chunkpos.x * 16 + x;
+                int realz = chunkpos.z * 16 + z;
+                int height = (int) (Math.tan(Math.sqrt((x - realz)^2 * (z - realx)^2)) * Math.PI);
+                for (int y = 1 ; y < height ; y++) {
+                    chunk.setBlockState(pos.setPos(x, y, z), stone, false);
+                }
+            }
+        }
+    }
+
+
 
 
     @Override
-    protected void fillNoiseColumn(double[] noiseColumn, int noiseX, int noiseZ) {
-        double d0 = 1368.824D;
-        double d1 = 684.412D;
-        double d2 = 17.110300000000002D;
-        double d3 = 4.277575000000001D;
-        int i = 64;
-        int j = -3000;
-        this.calcNoiseColumn(noiseColumn, noiseX, noiseZ, 1368.824D, 684.412D, 17.110300000000002D, 3.277575000000001D, 1, -2000);
+    public void makeBase(IWorld worldIn, IChunk chunkIn) {
+
     }
 
-    protected double[] getBiomeNoiseColumn(int noiseX, int noiseZ) {
-        return new double[]{(double)this.biomeProvider.func_222365_c(noiseX, noiseZ), 0.0D};
-    }
-
-    protected double func_222545_a(double p_222545_1_, double p_222545_3_, int p_222545_5_) {
-        return 8.0D - p_222545_1_;
-    }
-
-    protected double func_222551_g() {
-        return (double)((int)super.func_222551_g() / 2);
-    }
-
-    protected double func_222553_h() {
-        return 8.0D;
+    @Override
+    public int getHeight(int x, int z, Heightmap.Type heightmapType) {
+        return 0;
     }
 
 
 
     @Override
     public int getGroundHeight() {
-        return 50;
+        return world.getSeaLevel()+1;
     }
 
-    @Override
-    public int getSeaLevel() {
-        return 1;
-    }
+
 }
