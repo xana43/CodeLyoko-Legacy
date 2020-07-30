fab7f6e602ac24a625f5118adbf79455da12fa87
xana43 <jaylen@woontech.com>
2020-07-29 23:26:06 -0400

    tried to add text to the custom story screen. It didn't work

--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/screens/StoryBookGUI.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/screens/StoryBookGUI.java
@@ -1,7 +1,12 @@
 package com.Ultra_Nerd.CodeLyokoRemake15.screens;
 
+import com.Ultra_Nerd.CodeLyokoRemake15.Base;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.screen.ReadBookScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -12,7 +17,7 @@ public class StoryBookGUI extends ReadBookScreen
 
     private static final int BUTTON_NEXT = 0;
     private static final int BUTTON_PREV = 1;
-
+    private TextFieldWidget story;
     private int pageIndex = 0;
 
 
@@ -31,6 +36,32 @@ public class StoryBookGUI extends ReadBookScreen
     @Override
     public void init(Minecraft p_init_1_, int p_init_2_, int p_init_3_) {
         super.init(p_init_1_, p_init_2_, p_init_3_);
+        Initall();
+    }
+
+    private void Initall()
+    {
+
+        int tx = this.width / 2 ;
+        int ty = this.height / 2;
+        FontRenderer gunship_font = this.getMinecraft().getFontResourceManager().getFontRenderer(new ResourceLocation(Base.MOD_ID + ":gunship"));
+        assert gunship_font != null;
+        this.story = new TextFieldWidget(gunship_font, tx, ty, 200, 33, I18n.format("gui.cm.StoryScreen"));
+        this.story.setEnableBackgroundDrawing(false);
+        this.story.setVisible(true);
+        this.story.setTextColor(16777215);
+        this.story.setFocused2(true);
+        this.story.setEnabled(true);
+        this.story.setCanLoseFocus(false);
+        this.story.active = true;
+        this.story.canWrite();
+        this.story.writeText("Entry 1: " + "\n" +
+                "07/29/2020" + "\n" +
+                "Jeremy Belpois" + "\n" + "01:00" + "\n" + "I'm not sure what happened, X.A.N.A. was defeated years ago" + "\n" +
+                "but this time he's done it, he's managed to destroy the world as we know it, but I survived because I tried to save everyone by putting them in lyoko"
+        + "\n" + "I'm not sure how long I have to be here, hopefully someone finds this, and finds the hidden instructions to rebuild the super calculator and reset this mess");
+
+
     }
 
     public int getPageIndex() {
