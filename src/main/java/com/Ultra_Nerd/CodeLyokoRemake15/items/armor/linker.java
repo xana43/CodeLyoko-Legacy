7cbcb48fe312b6962d1457abd5c24b8b83913b3e
xana43 <jaylen@woontech.com>
2020-08-04 03:50:44 -0400

    added a durability mechanic to the linker armor item

--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/items/armor/linker.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/items/armor/linker.java
@@ -1,21 +1,33 @@
 package com.Ultra_Nerd.CodeLyokoRemake15.items.armor;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class linker extends ArmorItem {
     public linker(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builder) {
         super(materialIn, slot, builder);
     }
 
+    @Override
+    public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+        if(stack.getDamage() < stack.getMaxDamage() - 1) {
+            stack.damageItem(1, player, null);
+        }
+    }
 
+    @Override
+    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
 
+        if(stack.getDamage() != 0 && itemSlot != EquipmentSlotType.CHEST.getIndex())
+        {
+            stack.damageItem(-1,(PlayerEntity)entityIn,null);
+        }
 
-
-
-
-
-
-
+        super.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
+    }
 }
