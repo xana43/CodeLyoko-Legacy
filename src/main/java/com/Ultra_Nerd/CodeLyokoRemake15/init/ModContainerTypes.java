2f32d988428f6e01f82f4abf1a2d711ab4f40d2b
xana43 <jaylen@woontech.com>
2020-08-22 10:57:04 -0400

    uncommented some stuff, and fixed an accidental deletion from the mod container types

--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/init/ModContainerTypes.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/init/ModContainerTypes.java
@@ -15,6 +15,8 @@ public class ModContainerTypes {
             .register("container_infusing", () -> IForgeContainerType.create(ContainerInfusing::new));
     public static final RegistryObject<ContainerType<TowerInterfaceContainer>> TOWER_INTERFACE_CONTAINER = CONTAINER_TYPES
             .register("tower_interface_container",() -> IForgeContainerType.create(TowerInterfaceContainer::new));
+    public static final RegistryObject<ContainerType<ContainerElectricInfusing>> CONTAINER_ELECTRIC_INFUSING = CONTAINER_TYPES
+            .register("container_electric_infusing",() -> IForgeContainerType.create(ContainerElectricInfusing::new));
     public static final RegistryObject<ContainerType<HologramContainer>> HOLOGRAM_CONTAINER = CONTAINER_TYPES
             .register("hologram_container", () -> IForgeContainerType.create(HologramContainer::new));
     //public static final RegistryObject<ContainerType<ReactorContainer>> COMPUTER_REACTOR_CONTAINER = CONTAINER_TYPES
--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/items/StoryBook.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/items/StoryBook.java
@@ -1,5 +1,7 @@
 package com.Ultra_Nerd.CodeLyokoRemake15.items;
 
+import com.Ultra_Nerd.CodeLyokoRemake15.screens.StoryBookGUI;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -36,11 +38,11 @@ public class StoryBook extends WrittenBookItem {
     @Nonnull
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, @Nonnull PlayerEntity playerIn, @Nonnull Hand handIn) {
-       /*if(!worldIn.isRemote) {
+       if(!worldIn.isRemote) {
            Minecraft.getInstance().displayGuiScreen(new StoryBookGUI());
        }
 
-        */
+
        ItemStack stack = playerIn.getHeldItem(handIn);
        playerIn.openBook(stack,handIn);
         playerIn.sendMessage(new StringTextComponent("doesn't work properly yet"));
