5932809486c58c49d7c3d16b521b60fb2566c8a6
xana43 <jaylen@woontech.com>
2020-09-14 01:26:32 -0400

    added JEI to the mod, will be useful for debugging recipes

--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/Util/compat/jei/Jeicompat.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/Util/compat/jei/Jeicompat.java
@@ -1,36 +1,22 @@
 package com.Ultra_Nerd.CodeLyokoRemake15.Util.compat.jei;
 
-public class Jeicompat {
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import net.minecraft.util.ResourceLocation;
 
-	/*
-	@Override
-	public void registerCategories(IRecipeCategoryRegistration registry) {
-		// TODO Auto-generated method stub
-		IModPlugin.super.registerCategories(registry);
-	}
-	
-	@Override
-	public void register(IModRegistry registry) {
-		// TODO Auto-generated method stub
-		IModPlugin.super.register(registry);
-	}
-	
-	public static String trantoloc(String key)
-	{
-		if(I18n.canTranslate(key)) return I18n.translateToLocal(key);
-		else return I18n.translateToFallback(key);
-	}
-	
-	public static String transtolocform(String key, Object... format)
-	{
-		String s = trantoloc(key);
-		try
-		{
-			return String.format(s,format);
-		} catch(IllegalFormatException e)
-		{
-			return "Format error: " + s;
-		}
-	}
-	*/
+import javax.annotation.Nonnull;
+
+@JeiPlugin
+public class Jeicompat implements IModPlugin {
+
+
+    @Nonnull
+    @Override
+    public ResourceLocation getPluginUid() {
+        return null;
+    }
+
+
+
+
 }
