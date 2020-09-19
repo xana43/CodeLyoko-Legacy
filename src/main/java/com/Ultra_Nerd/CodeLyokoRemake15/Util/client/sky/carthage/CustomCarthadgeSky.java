7028cc357b0ee98e76e0be776f82b6fe9ae9ae40
xana43 <jaylen@woontech.com>
2020-09-19 11:42:38 -0400

    tried to add a custom skybox to sector 5

---
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/Util/client/sky/carthage/CustomCarthadgeSky.java
@@ -0,0 +1,29 @@
+package com.Ultra_Nerd.CodeLyokoRemake15.Util.client.sky.carthage;
+
+import com.Ultra_Nerd.CodeLyokoRemake15.Base;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.SkyRenderHandler;
+
+public class CustomCarthadgeSky implements SkyRenderHandler {
+
+
+    private ResourceLocation texturelocation = null;
+
+    public CustomCarthadgeSky() {
+
+        texturelocation = new ResourceLocation(Base.MOD_ID,"textures/skies/tower_wall.png");
+
+    }
+
+
+    @Override
+    public void render(int ticks, float partialTicks, MatrixStack matrixStack, ClientWorld world, Minecraft mc) {
+  //  world.dimension.getSkyRenderer().render(ticks, partialTicks, world, mc);
+        mc.textureManager.bindTexture(texturelocation);
+
+
+    }
+}
--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/world/dimension/Carthage/Sector5Dimension.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/world/dimension/Carthage/Sector5Dimension.java
@@ -1,5 +1,6 @@
 package com.Ultra_Nerd.CodeLyokoRemake15.world.dimension.Carthage;
 
+import com.Ultra_Nerd.CodeLyokoRemake15.Util.client.sky.carthage.CustomCarthadgeSky;
 import com.Ultra_Nerd.CodeLyokoRemake15.world.generators.Sector5Generator;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
@@ -12,15 +13,16 @@ import net.minecraftforge.client.IRenderHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Random;
 
 public class Sector5Dimension extends Dimension {
 
+    private static CustomCarthadgeSky carthadgeSky = null;
     public static final BlockPos SPAWN = new BlockPos(0, 128, 0);
 
     public Sector5Dimension(World world, DimensionType type) {
         super(world, type, 0.0f);
-
+        carthadgeSky = new CustomCarthadgeSky();
+        this.setSkyRenderer(carthadgeSky);
     }
 
     @Nonnull
@@ -30,15 +32,17 @@ public class Sector5Dimension extends Dimension {
     }
 
 
+
     @Override
     @Nullable
-    public BlockPos findSpawn(ChunkPos chunkPosIn, boolean checkValid) {
+    public BlockPos findSpawn(@Nonnull ChunkPos chunkPosIn, boolean checkValid) {
         /*Random random = new Random(this.world.getSeed());
         BlockPos blockpos = new BlockPos(chunkPosIn.getXStart() + random.nextInt(15), 0, chunkPosIn.getZEnd() + random.nextInt(15));
         return this.world.getGroundAboveSeaLevel(blockpos).getMaterial().blocksMovement() ? blockpos : null;*/
         return SPAWN;
     }
 
+    @Override
     public BlockPos getSpawnCoordinate() {
         return SPAWN;
     }
@@ -80,9 +84,10 @@ public class Sector5Dimension extends Dimension {
         return false;
     }
 
+
     @Nullable
     @Override
     public IRenderHandler getSkyRenderer() {
-        return super.getSkyRenderer();
+        return carthadgeSky;
     }
 }
