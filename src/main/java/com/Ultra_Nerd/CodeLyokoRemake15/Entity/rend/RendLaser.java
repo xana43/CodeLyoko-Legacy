108aeb5f24bb7c3425a99207c741946a0edd532f
xana43 <jaylen@woontech.com>
2020-09-22 05:23:22 -0400

    tweaked laser arrow renderer to use it's custom model

--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/Entity/rend/RendLaser.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/Entity/rend/RendLaser.java
@@ -6,17 +6,19 @@ import com.Ultra_Nerd.CodeLyokoRemake15.Entity.model.ModelLaserArrow;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.entity.ArrowRenderer;
+import net.minecraft.client.renderer.Vector3f;
+import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 import javax.annotation.Nonnull;
 
 @OnlyIn(Dist.CLIENT)
-public class RendLaser<T extends EntityLaser> extends ArrowRenderer<T> {
+public class RendLaser<T extends EntityLaser> extends EntityRenderer<T> {
     private final ModelLaserArrow<T> laserArrow = new ModelLaserArrow<>();
     public RendLaser(EntityRendererManager rendManIn) {
         super(rendManIn);
@@ -27,11 +29,38 @@ public class RendLaser<T extends EntityLaser> extends ArrowRenderer<T> {
     public void render(@Nonnull T entityIn, float entityYaw, float partialTicks, @Nonnull MatrixStack matrixStackIn, @Nonnull IRenderTypeBuffer bufferIn, int packedLightIn) {
         super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
         matrixStackIn.push();
-        laserArrow.setRotationAngles(entityIn,0,0,0,0,0);
-        IVertexBuilder vertexBuilder = bufferIn.getBuffer(this.laserArrow.getRenderType(this.getEntityTexture(entityIn)));
-        laserArrow.render(matrixStackIn,vertexBuilder,packedLightIn, OverlayTexture.NO_OVERLAY,1,1,1,1);
+        IVertexBuilder ivertexbuilder = bufferIn.getBuffer(this.laserArrow.getRenderType(this.getEntityTexture(entityIn)));
+        this.laserArrow.render(matrixStackIn,ivertexbuilder,packedLightIn, OverlayTexture.NO_OVERLAY,1,1,1,1);
+        matrixStackIn.rotate(Vector3f.YP.rotationDegrees(MathHelper.lerp(partialTicks, entityIn.prevRotationYaw, entityIn.rotationYaw) - 90.0F));
+        matrixStackIn.rotate(Vector3f.ZP.rotationDegrees(MathHelper.lerp(partialTicks, entityIn.prevRotationPitch, entityIn.rotationPitch)));
+        int i = 0;
+        float f = 0.0F;
+        float f1 = 0.5F;
+        float f2 = 0.0F;
+        float f3 = 0.15625F;
+        float f4 = 0.0F;
+        float f5 = 0.15625F;
+        float f6 = 0.15625F;
+        float f7 = 0.3125F;
+        float f8 = 0.05625F;
+        float f9 = (float)entityIn.arrowShake - partialTicks;
+        if (f9 > 0.0F) {
+            float f10 = -MathHelper.sin(f9 * 3.0F) * f9;
+            matrixStackIn.rotate(Vector3f.ZP.rotationDegrees(f10));
+        }
+
+        matrixStackIn.rotate(Vector3f.XP.rotationDegrees(45.0F));
+        //matrixStackIn.scale(0.05625F, 0.05625F, 0.05625F);
+        //matrixStackIn.translate(-4.0D, 0.0D, 0.0D);
+
+
+
         matrixStackIn.pop();
+        super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
     }
+
+
+
 
     @Nonnull
     @Override
