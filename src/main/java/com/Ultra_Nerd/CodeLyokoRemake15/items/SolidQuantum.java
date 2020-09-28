1dc5d19d57242d1a56e96e7c367a23241ef82c87
xana43 <jaylen@woontech.com>
2020-09-28 16:29:26 -0400

    tried to make solid quantum act like netherite, dont know how to do that

--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/items/SolidQuantum.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/items/SolidQuantum.java
@@ -1,16 +1,44 @@
 package com.Ultra_Nerd.CodeLyokoRemake15.items;
 
+import com.Ultra_Nerd.CodeLyokoRemake15.Base;
 import net.minecraft.item.Item;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.item.ItemEvent;
+import net.minecraftforge.event.entity.item.ItemExpireEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = Base.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class SolidQuantum extends Item {
     public SolidQuantum(Properties properties) {
         super(properties);
+
     }
+
+
 
     @Override
     public boolean isDamageable() {
         return false;
     }
+@SubscribeEvent
+public void ItemInvulnerable(ItemExpireEvent event)
+{if(event.getEntityItem().getItem().getItem() == this)
+    {
 
-    
+        event.getEntityItem().isInvulnerableTo(DamageSource.LAVA);
+
+    }
+}
+    @SubscribeEvent
+    public void ItemInvulnerable2(ItemEvent event)
+    {if(event.getEntityItem().getItem().getItem() == this)
+    {
+
+        event.getEntityItem().isInvulnerableTo(DamageSource.LAVA);
+
+    }
+    }
+
 }
