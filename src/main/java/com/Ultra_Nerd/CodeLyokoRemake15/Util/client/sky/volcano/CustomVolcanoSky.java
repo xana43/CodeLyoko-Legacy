e21cbda108ed061c5a9e014389a0500801237642
xana43 <jaylen@woontech.com>
2020-09-30 04:56:29 -0400

    reverted volcano sky to a non-broken state(this time for real)

--- src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/Util/client/sky/volcano/CustomVolcanoSky.java
+++ src/main/java/com/Ultra_Nerd/CodeLyokoRemake15/Util/client/sky/volcano/CustomVolcanoSky.java
@@ -1,5 +1,7 @@
 package com.Ultra_Nerd.CodeLyokoRemake15.Util.client.sky.volcano;
 
+
+
 import com.Ultra_Nerd.CodeLyokoRemake15.Base;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
@@ -13,7 +15,6 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.SkyRenderHandler;
-
 @OnlyIn(Dist.CLIENT)
 public class CustomVolcanoSky implements SkyRenderHandler {
 
@@ -21,12 +22,11 @@ public class CustomVolcanoSky implements SkyRenderHandler {
     private static ResourceLocation sky1 = null;
     private static ResourceLocation sky2 = null;
     private static ResourceLocation skytop = null;
-
     public CustomVolcanoSky() {
 
-        sky1 = new ResourceLocation(Base.MOD_ID, "textures/skies/volcano/sky1.png");
-        sky2 = new ResourceLocation(Base.MOD_ID, "textures/skies/volcano/sky2.png");
-        skytop = new ResourceLocation(Base.MOD_ID, "textures/skies/volcano/dark.png");
+        sky1 = new ResourceLocation(Base.MOD_ID,"textures/skies/volcano/sky1.png");
+        sky2 = new ResourceLocation(Base.MOD_ID,"textures/skies/volcano/sky2.png");
+        skytop = new ResourceLocation(Base.MOD_ID,"textures/skies/volcano/dark.png");
     }
 
 
@@ -36,92 +36,46 @@ public class CustomVolcanoSky implements SkyRenderHandler {
         mc.textureManager.bindTexture(skytop);
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferBuilder = tessellator.getBuffer();
-        for (int i = 0; i < 6; ++i) {
+        for(int i = 0; i < 6; ++i) {
             matrixStack.push();
-<<<<<<< HEAD
-<<<<<<< HEAD
-            if (i == 0) {
-                matrixStack.scale(10, 10, 10);
-=======
-                if (i == 0) {
-                    matrixStack.scale(10, 10, 10);
->>>>>>>parent of a7c362f...reformatted all code using intellij 's reformat tool
-                }
-=======
->>>>>>>parent of b13df97...tried to move the skybox upwards to show the texture better
-                if (i == 1) {
-                    mc.textureManager.bindTexture(sky1);
-                    matrixStack.rotate(Vector3f.XP.rotationDegrees(90.0F));
-                    matrixStack.rotate(Vector3f.YP.rotationDegrees(0));
-                }
-=======
-=======
->>>>>>> parent of b13df97... tried to move the skybox upwards to show the texture better
             if (i == 1) {
                 mc.textureManager.bindTexture(sky1);
                 matrixStack.rotate(Vector3f.XP.rotationDegrees(90.0F));
                 matrixStack.rotate(Vector3f.YP.rotationDegrees(0));
             }
->>>>>>> parent of b13df97... tried to move the skybox upwards to show the texture better
 
-                if (i == 2) {
-                    mc.textureManager.bindTexture(sky2);
-                    matrixStack.rotate(Vector3f.XP.rotationDegrees(-90.0F));
-                    matrixStack.rotate(Vector3f.YP.rotationDegrees(180));
-                }
+            if (i == 2) {
+                mc.textureManager.bindTexture(sky2);
+                matrixStack.rotate(Vector3f.XP.rotationDegrees(-90.0F));
+                matrixStack.rotate(Vector3f.YP.rotationDegrees(180));
+            }
 
-<<<<<<< HEAD
-                if (i == 3) {
-                    mc.textureManager.bindTexture(skytop);
-                    matrixStack.rotate(Vector3f.XP.rotationDegrees(180.0F));
-<<<<<<<HEAD
-                            <<<<<<<HEAD
-                            matrixStack.scale(10, 10, 10);
-=======
->>>>>>>parent of b13df97...tried to move the skybox upwards to show the texture better
-                            =======
-                    matrixStack.scale(10, 10, 10);
->>>>>>>parent of a7c362f...reformatted all code using intellij 's reformat tool
-                }
-=======
             if (i == 3) {
                 mc.textureManager.bindTexture(skytop);
                 matrixStack.rotate(Vector3f.XP.rotationDegrees(180.0F));
             }
->>>>>>> parent of b13df97... tried to move the skybox upwards to show the texture better
 
-                if (i == 4) {
-                    mc.textureManager.bindTexture(sky2);
-                    matrixStack.rotate(Vector3f.ZP.rotationDegrees(90.0F));
-                    matrixStack.rotate(Vector3f.YP.rotationDegrees(-90));
-                }
-
-                if (i == 5) {
-                    mc.textureManager.bindTexture(sky1);
-                    matrixStack.rotate(Vector3f.ZP.rotationDegrees(-90.0F));
-                    matrixStack.rotate(Vector3f.YP.rotationDegrees(90));
-                }
-
-                Matrix4f matrix4f = matrixStack.getLast().getMatrix();
-                bufferBuilder.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
-                bufferBuilder.pos(matrix4f, -100.0F, -100.0F, -100.0F).tex(0.0F, 0.0F).color(40, 40, 40, 255).endVertex();
-                bufferBuilder.pos(matrix4f, -100.0F, -100.0F, 100.0F).tex(0.0F, 1.0F).color(40, 40, 40, 255).endVertex();
-                bufferBuilder.pos(matrix4f, 100.0F, -100.0F, 100.0F).tex(1.0F, 1.0F).color(40, 40, 40, 255).endVertex();
-                bufferBuilder.pos(matrix4f, 100.0F, -100.0F, -100.0F).tex(1.0F, 0.0F).color(40, 40, 40, 255).endVertex();
-                tessellator.draw();
-                matrixStack.pop();
+            if (i == 4) {
+                mc.textureManager.bindTexture(sky2);
+                matrixStack.rotate(Vector3f.ZP.rotationDegrees(90.0F));
+                matrixStack.rotate(Vector3f.YP.rotationDegrees(-90));
             }
 
-<<<<<<< HEAD
-=======
+            if (i == 5) {
+                mc.textureManager.bindTexture(sky1);
+                matrixStack.rotate(Vector3f.ZP.rotationDegrees(-90.0F));
+                matrixStack.rotate(Vector3f.YP.rotationDegrees(90));
+            }
+
             Matrix4f matrix4f = matrixStack.getLast().getMatrix();
             bufferBuilder.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
             bufferBuilder.pos(matrix4f, -100.0F, -100.0F, -100.0F).tex(0.0F, 0.0F).color(40, 40, 40, 255).endVertex();
             bufferBuilder.pos(matrix4f, -100.0F, -100.0F, 100.0F).tex(0.0F, 1.0F).color(40, 40, 40, 255).endVertex();
-            bufferBuilder.pos(matrix4f, 100.0F, -100.0F, 100.0F).tex(1.0F, 1.0F).color(40, 40, 40, 255).endVertex();
-            bufferBuilder.pos(matrix4f, 100.0F, -100.0F, -100.0F).tex(1.0F, 0.0F).color(40, 40, 40, 255).endVertex();
+            bufferBuilder.pos(matrix4f, 100.0F, -90.0F, 100.0F).tex(1.0F, 1.0F).color(40, 40, 40, 255).endVertex();
+            bufferBuilder.pos(matrix4f, 100.0F, -90.0F, -100.0F).tex(1.0F, 0.0F).color(40, 40, 40, 255).endVertex();
             tessellator.draw();
             matrixStack.pop();
->>>>>>> parent of b13df97... tried to move the skybox upwards to show the texture better
         }
+
     }
+}
